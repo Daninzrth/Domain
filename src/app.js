90f9873c55186365df7971a71e86fb23fa8fb650
Daninzrth <dani.angulo1910@gmail.com>
2022-03-25 19:54:54 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = GetDomain();
 };
+
+function GetDomain() {
+  var pronoun = ["the", "our", "my"];
+  var adj = ["great", "big", "pretty", "cute"];
+  var noun = ["jogger", "racoon", "dog", "butterfly"];
+  var service = [".com", ".net", ".es"];
+
+  return (
+    pronoun[Math.floor(Math.random() * (pronoun.length - 1))] +
+    adj[Math.floor(Math.random() * (adj.length - 1))] +
+    noun[Math.floor(Math.random() * (noun.length - 1))] +
+    service[Math.floor(Math.random() * (service.length - 1))]
+  );
+}
